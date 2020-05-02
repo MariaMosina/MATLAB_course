55b2747516c57e07a3cf5cfbea120d5b998accf9
MariaMosina <maria-mosina@mail.ru>
2020-05-02 17:20:19 +0300

    Adding hw_1

--- hw_1.m
+++ hw_1.m
@@ -1,3 +1,6 @@
 M1 = matrix_chess(5,7);
 M2 = alternation_columns(5, 7);
-M3 = matrix_with_circle(9, 2)
+M3 = matrix_with_circle(9, 2);
+M4 = zigzag(5);
+M5 = random_int(5, 1, 10);
+M6 = increase_one(5, 6, 2)
