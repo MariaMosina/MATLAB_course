9da3a25eb4c9f37a8a76b5ff594160507b8a1d3a
MariaMosina <maria-mosina@mail.ru>
2020-08-03 16:08:36 +0300

    adding last files

--- hw_6.m
+++ hw_6.m
@@ -1,6 +1,6 @@
 phi = -pi/2:pi/50:pi/2;
 teta = 0:pi/25:2*pi;
-h = surf(sin(teta)'*cos(phi), sin(teta)'*sin(phi), cos(teta)'*cos(teta));
+h = surf(sin(teta)'*cos(phi), sin(teta)'*sin(phi), cos(teta)'*sin(teta));
 figure;
 ax1 = subplot(2, 2, 1); 
 ax2 = subplot(2, 2, 2); 
